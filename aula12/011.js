a8cf4bd8f9480e4b5cc3b966fb5048f3c44d9053
DenisRFerro <denis.rferro@hotmail.com>
2022-01-18 18:38:29 -0300

    aad

---
+++ aula12/011.js
@@ -0,0 +1,10 @@
+var idade = 65
+console.log (`Com ${idade} anos de idade o: `)
+if (idade < 16) {
+    console.log ('Não vota!')    
+} else if (idade < 18 || idade > 65) {
+    console.log ('Voto é opicional!')    
+} else  {
+    console.log ('Voto obrigatório')
+    
+}
