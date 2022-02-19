5cb2d2f3049847845d0e4cfd0d4828d6c0b4700d
DenisRFerro <denis.rferro@hotmail.com>
2022-02-18 22:13:48 -0300

    ultimos retoques en HTML e iniciando javascript

---
+++ calculadora/calculadora.js
@@ -0,0 +1,10 @@
+const res = document.querySelector('#res')
+const add= document.querySelector('#view2')
+const resultado = document.querySelector('#view1')
+
+function insert(value){
+
+    document.querySelector('#view2').innerHTML += value
+
+}
+ 
